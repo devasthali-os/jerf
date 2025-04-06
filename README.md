ad76ec6050c265985fa4532b27a316a47e980b6f
parayaluyanta <prayag.upd@gmail.com>
2025-04-06 11:43:07 -0700

    add memory usage

--- README.md
+++ README.md
@@ -11,13 +11,31 @@ basic java perf examples
 
 - find unique ids in "1 million" records - 100ms+
 
+```bash
+Memory Usage Before Preparing List:
+Total Memory: 260 mb
+Free Memory: 257 mb
+Used Memory: 3 mb
+
+Memory Usage After Preparing List:
+Total Memory: 1748 mb
+Free Memory: 1313 mb
+Used Memory: 435 mb
+
+Unique adIds 10000
+Unique search time taken 110 ms
+Memory Usage After Processing List:
+Total Memory: 1748 mb
+Free Memory: 1313 mb
+Used Memory: 435 mb
+```
 
 jmeter
 --------
 
-
 - https://jmeter.apache.org/download_jmeter.cgi
 
 
-
-database-perf - https://github.com/prayagupd/database-entertainment
+database-perf
+---
+https://github.com/prayagupa/rdb
--- src/main/java/com/upadhyay/jerf/App.java
+++ src/main/java/com/upadhyay/jerf/App.java
@@ -1,5 +1,5 @@
 package com.upadhyay.jerf;
-
+import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +8,13 @@ import java.util.stream.IntStream;
 
 public class App {
 
-    public static final int BILLION = 1_000_000_000;
+    public static final int FIVE_MILL = 5_000_000;
     public static final int MILLION = 1_000_000;
 
     public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("Initial Memory Usage:");
+        printMemoryUsage(runtime);
 
         var s = IntStream.range(1, 10).collect(
                 () -> new StringBuilder(),
@@ -22,12 +25,16 @@ public class App {
 
         System.out.println(s);
 
-        listPerf();
+        listPerf(runtime);
     }
 
-    public static void listPerf() {
+    public static void listPerf(Runtime runtime) {
+        var prepStart = System.currentTimeMillis();
 
-        List<Ad> ads = IntStream.range(1, BILLION).collect(
+        System.out.println("Memory Usage Before Preparing List:");
+        printMemoryUsage(runtime);
+
+        List<Ad> ads = IntStream.range(1, MILLION).collect(
                 () -> new ArrayList<Ad>(),
                 (acc, elem) -> acc.add(
                         new Ad(
@@ -42,14 +49,30 @@ public class App {
         );
 
         var start = System.currentTimeMillis();
+//        System.out.println("Prep time taken " + (start - prepStart) + " ms");
+
+        System.out.println("Memory Usage After Preparing List:");
+        printMemoryUsage(runtime);
 
         var adIds = ads.stream().map($ -> $.adId);
         var uniqueAdIds = adIds.distinct();
 
-        System.out.println("unique adIds " + uniqueAdIds.count());
+        System.out.println("Unique adIds " + uniqueAdIds.count());
         var timeTaken = System.currentTimeMillis() - start;
 
-        System.out.println("time taken " + timeTaken + " ms"); //time taken 107 ms
+        System.out.println("Unique search time taken " + timeTaken + " ms");
+        System.out.println("Memory Usage After Processing List:");
+        printMemoryUsage(runtime);
     }
 
+    public static void printMemoryUsage(Runtime runtime) {
+        long totalMemory = runtime.totalMemory() / (1024 * 1024);
+        long freeMemory = runtime.freeMemory() / (1024 * 1024);
+        long usedMemory = (totalMemory - freeMemory);
+
+        System.out.printf("Total Memory: %d mb%n", totalMemory);
+        System.out.printf("Free Memory: %d mb%n", freeMemory);
+        System.out.printf("Used Memory: %d mb%n", usedMemory);
+        System.out.println();
+    }
 }
